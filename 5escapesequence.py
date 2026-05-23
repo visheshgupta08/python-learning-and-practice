24211b5d0559576e20ccb7b7fb9c50c1db99a177
visheshgupta08 <visheshgupta298@gmail.com>
2026-05-23 23:35:53 +0530

    Added escape sequence file

---
+++ 5escapesequence.py
@@ -0,0 +1,15 @@
+# print("hey i am a good boy
+# and this viewer is also a good boy ") error thi isme 
+# \n ka matlab hai 'New Line'—ye text ko agali line par bhej deta hai
+print("hey i am a good boy \n and this viewer is also a good boy") 
+# agr triple ''' ke bich likho toh bhi vo comment hi hota h usme error nahi aati h
+#print("hey i am a "good boy"\n and this viewer is also a good boy") error thi 
+# \" ka use tab karte hain jab humein string ke andar hi double quotes dikhane honge
+print("hey i am a \"good boy\"\n and this viewer is also a good boy")
+
+# sep="~" ka matlab hai ki items (hey, 6, 7) ke beech mein space ki jagah ~ aayega
+# end="009\n" ka matlab hai ki line ke khatam hote hi 009 print hoga aur fir nayi line aayegi
+print("hey", 6, 7, sep="~", end="009\n")
+
+# Yeh simple print hai, jo pichle wale 'end' argument ki wajah se 009 ke baad wali line mein dikhega
+print("vishesh")
