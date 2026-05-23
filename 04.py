01dd91ee10c3700099ff1b23be2920f641d8ba08
Vishesh Gupta <visheshgupta298@gmail.com>
2026-05-24 00:01:30 +0530

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,12 +1,12 @@
-print("hello world", 7)
 # Isme string "hello world" aur number 7 dono ek saath print honge
+print("hello world", 7)
 
-print(5)
 # Yeh seedha number 5 ko terminal pe dikha dega
+print(5)
 
-print("bye")
 # Yeh ek simple string message print karega: bye
+print("bye")
 
-print(21*2)
 # Python yahan pehle calculation (21 multiply by 2) karega, 
 # phir result (42) print karega
+print(21*2)
