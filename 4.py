b3d9001dbbd517f840e9f9bae412fe34dec214ce
visheshgupta08 <visheshgupta298@gmail.com>
2026-05-23 23:34:06 +0530

    Added Python introduction file

---
+++ 4.py
@@ -0,0 +1,12 @@
+print("hello world", 7)
+# Isme string "hello world" aur number 7 dono ek saath print honge
+
+print(5)
+# Yeh seedha number 5 ko terminal pe dikha dega
+
+print("bye")
+# Yeh ek simple string message print karega: bye
+
+print(21*2)
+# Python yahan pehle calculation (21 multiply by 2) karega, 
+# phir result (42) print karega
