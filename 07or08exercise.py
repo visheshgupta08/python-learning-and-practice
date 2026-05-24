d87a707a83d98794720deee398b1b640aa0acdfb
visheshgupta08 <visheshgupta298@gmail.com>
2026-05-24 12:31:55 +0530

    Added Python Calculator exercise file

---
+++ 07or08exercise.py
@@ -0,0 +1,32 @@
+# Addition: Simple 5 aur 6 ko jod kar 11 dega
+print(5+6)
+
+# Subtraction: 15 mein se 6 ghata kar 9 dega
+print(15-6)
+
+# Multiplication: 15 aur 6 ko guna karke 90 dega
+print(15*6)
+
+# Division: Yeh hamesha float (decimal) mein answer dega, jaise 2.5
+print(15/6)
+
+# Floor Division: Yeh decimal hata deta hai aur sirf integer part deta hai (2.5 ka sirf 2)
+print(15//6)
+
+# Modulo Operator: Yeh division ke baad bacha hua 'Remainder' (sheshphal) batata hai
+# 5 ko 3 se divide karo toh remainder 2 bachega
+print(5%3)
+
+# Exponential: Iska matlab hai 2 ki power 4 (2*2*2*2), answer aayega 16
+print(2**4)
+
+
+
+# exercise:Calculator
+a=50 
+b=3 
+
+print("the value of", a , "+", 3 , "is: " , a+b )
+print("the value of", a , "-", 3 , "is: " , a-b )
+print("the value of", a , "*", 3 , "is: " , a*b )
+print("the value of", a , "/", 3 , "is: " , a/b )
